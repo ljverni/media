8157d851fb65ef4f63a796e2907726bf87c8c186
Luciano Verni <lucianoverni@gmail.com>
2020-06-01 00:19:49 +0200

    nacion_scroll reduced sleep time

--- nacion_infinitescroll.py
+++ nacion_infinitescroll.py
@@ -14,7 +14,7 @@ def scroll_down(base_url):
     driver.get(base_url)
     for i in range(1,20):
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(4)
+        time.sleep(3)
     elems = driver.find_elements_by_xpath("//a[@href]")
     urls = []
     for elem in elems:
